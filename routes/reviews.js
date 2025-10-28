016024a44c34cbe771b8fbb798ab6f96e8e6123d
paraggarg969 <paraggarg969@gmail.com>
2025-10-29 02:04:54 +0530

    deploy

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,16 +1,31 @@
 const express = require("express");
-const router = express.Router({mergeParams: true});
+const router = express.Router({ mergeParams: true });
 const wrapAsync = require("../utils/wrapAsyc.js");
 const Review = require("../models/review.js");
 const Listing = require("../models/listing.js");
-const {validateReview, isLoggedIn, isOwner, isreviewAuthor} = require("../middleware.js");
+const {
+  validateReview,
+  isLoggedIn,
+  isOwner,
+  isreviewAuthor,
+} = require("../middleware.js");
 
 const reviewController = require("../controllers/review.js");
 
 //Reviews  // POST route
-router.post("/", isLoggedIn, validateReview, wrapAsync(reviewController.createReview));
-  
-  // delete review route
-router.delete("/:reviewId", isLoggedIn, isreviewAuthor, wrapAsync(reviewController.deletereview));
+router.post(
+  "/",
+  isLoggedIn,
+  validateReview,
+  wrapAsync(reviewController.createReview)
+);
 
-  module.exports = router;
+// delete review route
+router.delete(
+  "/:reviewId",
+  isLoggedIn,
+  isreviewAuthor,
+  wrapAsync(reviewController.deletereview)
+);
+
+module.exports = router;
